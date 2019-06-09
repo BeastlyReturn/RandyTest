828f2bd2eceafaf2c9ca84615f9853f1e07b649c
Beastly <info@phales.net>
2019-06-09 22:53:17 +0200

    First Commit

---
+++ src/main/java/de/beastly/RandyTest/RandyTest.java
@@ -0,0 +1,17 @@
+package de.beastly.RandyTest;
+
+        import org.bukkit.plugin.java.JavaPlugin;
+
+public class RandyTest extends JavaPlugin {
+
+        @Override
+        public void onEnable() {
+
+        }
+
+        @Override
+        public void onDisable() {
+
+
+        }
+}
